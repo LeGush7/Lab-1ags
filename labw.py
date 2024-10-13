b441907a8d2113986ff176eca73cb912fd599212
LeGush <alekseevgordey@gmail.com>
2024-10-13 19:15:10 +0300

    вариант с исправлениями

--- labw.py
+++ labw.py
@@ -1,45 +1,55 @@
 # задание №1
-print('\x1b[48;2;250;185;0m            \x1b[0m')
-print('\x1b[48;2;9;101;64m            \x1b[0m')
-print('\x1b[48;2;190;42;42m   \x1b[38;2;190;42;42m         \x1b[0m')
-# цельный пробел разделён на два, т.к. цельный не выводится; если уменьшить общую длинну, то нарушится пропорция 3:5
+inputheight = int(input('Введите высоту флага: '))
+heightcolour = (inputheight + 1) // 3 if inputheight > 2 else 1
+for i in range(heightcolour):
+    print('\x1b[48;2;250;185;0m ' * int((heightcolour // 0.1)), '\x1b[0m')
+for i in range(heightcolour):
+    print('\x1b[48;2;9;101;64m ' * int((heightcolour // 0.1)), '\x1b[0m')
+for i in range(heightcolour):
+    print('\x1b[48;2;190;42;42m ' * int((heightcolour // 0.1)), '\x1b[0m')
 
 # задание №2
-st = 7
-height = st + 1
-kz = st - 1
-prob = 0
-pr = [int(x - 1) for x in range(st + 1, 2, -1)]
-print(st * '\x1b[48;5;27;1m \x1b[0m' + (sum(pr) * 2 - st * 2 + 1) * ' ' + st * '\x1b[48;5;27;1m \x1b[0m')
+STARTLENGTH = 7
+height = STARTLENGTH + 1
+filledcells = STARTLENGTH - 1
+emptycells = 0
+numsoffcells = [int(x - 1) for x in range(STARTLENGTH + 1, 2, -1)]
+print(STARTLENGTH * '\x1b[48;5;27;1m \x1b[0m' + (sum(numsoffcells) * 2
+     - STARTLENGTH * 2 + 1) * ' ' +
+     STARTLENGTH * '\x1b[48;5;27;1m \x1b[0m')
 for h in range(height):
-    if kz != 1:
-        prob += 1
-        print(' ' * sum(pr[0:prob]) + '\x1b[48;5;27;1m \x1b[0m' * pr[prob] + (sum(pr) * 2 -
-                    (sum(pr[0:prob + 1])) * 2 + 1) * ' ' + '\x1b[48;5;27;1m \x1b[0m' * pr[prob] + ' ' * sum(pr[0:prob]))
-        kz -= 1
+    if filledcells != 1:
+        emptycells += 1
+        print(' ' * sum(numsoffcells[0:emptycells]) +
+              '\x1b[48;5;27;1m \x1b[0m' * numsoffcells[emptycells] +
+              (sum(numsoffcells) * 2 - (sum(numsoffcells[0:emptycells +
+              1])) * 2 + 1) * ' ' + '\x1b[48;5;27;1m \x1b[0m' *
+              numsoffcells[emptycells] +
+              ' ' * sum(numsoffcells[0:emptycells]))
+        filledcells -= 1
     else:
-        print((sum(pr)) * ' ' + '\x1b[48;5;27;1m \x1b[0m' + (sum(pr)) * ' ')
+        print((sum(numsoffcells)) * ' ' +
+              '\x1b[48;5;27;1m \x1b[0m' + (sum(numsoffcells)) * ' ')
 
 # задание №3
-h, w = 9, 9
-c = 1
-for y in range(h):
-    print((w - c) * ' ' + '\x1b[48;5;27;1m \x1b[0m')
-    c += 1
+HEIGTH = 9
+WIDTH = 9
+lastcells = 1
+for y in range(HEIGTH):
+    print((WIDTH - lastcells) * ' ' + '\x1b[48;5;27;1m \x1b[0m')
+    lastcells += 1
 
 # задание №4
-import matplotlib.pyplot as p
-
-
-vlist = [abs(float(x)) for x in open('sequence.txt')]
-v1, v2 = 0, 0
-for x in range(0, len(vlist), 2):
-    v1 += vlist[x]
-for x in range(1, len(vlist), 2):
-    v2 += vlist[x]
-v2 = round(v2, 1)
-vs = [v1, v2]
-nms = ['Чётные', 'Нечётные']
-p.pie(vs, labels=nms, autopct=lambda p: '{:.2f}%'.format(p), startangle=90, colors=['#66ff66','#ffcc66'])
-p.title('Процентное соотношение')
-p.show()
+with open("sequence.txt") as sequence:
+    data = sequence.read()
+valueslist = [abs(float(num)) for num in data.split('\n')]
+oddvalue, notoddvalue = 0, 0
+for i in range(0, len(valueslist), 2):
+    notoddvalue += valueslist[i]
+for i in range(1, len(valueslist), 2):
+    oddvalue += valueslist[i]
+allvalues = oddvalue + notoddvalue
+procentodd = round((oddvalue / allvalues) * 100)
+procentnotodd = round((notoddvalue / allvalues) * 100)
+print('Нечёт. позиции', '\x1b[48;5;27;10m ' * procentodd, '\x1b[0m')
+print('Чёт. позиции  ', '\x1b[48;5;27;10m ' * procentnotodd, '\x1b[0m')
